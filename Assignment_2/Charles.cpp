d62cb2cc448fcd5e9e2d8abe51ddae5240f0dd42
Patrick Hilhorst <patrickhilhorst97@gmail.com>
2015-09-14 19:44:36 +0200

    Updated formatting

--- Assignment_2/Charles.cpp
+++ Assignment_2/Charles.cpp
@@ -1,4 +1,5 @@
 #include "Robot.h"
+
 /*
 	Karel is a framework for manipulating with Karel the robot.
 	Karel is a subclass of ROBOT from the library Robot.
@@ -11,17 +12,16 @@
 // =========================================================================
 
 // make_church creates a Church-like shape of walls
-void make_church ()
-{
-    const int lane   = 2 + rand () % (WereldHoogte / 5) ;
-    const int street = 2 + rand () % (WereldBreedte / 5) ;
-    const int width  = 3 + rand () % (WereldBreedte / 2) ;
-    const int height = 2 + rand () % (WereldHoogte / 3) ;
-    place_rectangle (street,lane,width,height) ;
-    place_rectangle (street + 2,lane + height, 2 + width / 3, 2 + height / 3) ;
-    place_walls (street + 3 + width / 6,lane + height + height / 3 + 3,4,false) ;
-    place_walls (street + 2 + width / 6,lane + height + height / 3 + 5,2,true) ;
-    create_ball (street, WereldHoogte - 2) ;
+void make_church() {
+	const int lane = 2 + rand() % (WereldHoogte / 5);
+	const int street = 2 + rand() % (WereldBreedte / 5);
+	const int width = 3 + rand() % (WereldBreedte / 2);
+	const int height = 2 + rand() % (WereldHoogte / 3);
+	place_rectangle(street, lane, width, height) ;
+	place_rectangle(street + 2, lane + height, 2 + width / 3, 2 + height / 3) ;
+	place_walls(street + 3 + width / 6, lane + height + height / 3 + 3, 4, false) ;
+	place_walls(street + 2 + width / 6, lane + height + height / 3 + 5, 2, true) ;
+	create_ball(street, WereldHoogte - 2) ;
 }
 
 
@@ -30,27 +30,23 @@ void make_church ()
 // Name / student number / study student 1 : Patrick Hilhorst / S4577434 / Computer Science
 // Name / student number / study student 2 : Fons van der Plas / S4576586 / Mathematics
 
-void turn_around()
-{
+void turn_around() {
 	turn_right();
 	turn_right();
 }
 
 // turn_north makes Charles look north
-void turn_north()
-{
-	while(!north())
+void turn_north() {
+	while (!north())
 		turn_left();
 }
 
-void walk_to_wall()
-{
-	while(!in_front_of_wall())
+void walk_to_wall() {
+	while (!in_front_of_wall())
 		step();
 }
 
-void go_to_north_west_corner()
-{
+void go_to_north_west_corner() {
 	turn_north();
 	walk_to_wall();
 	turn_left();
@@ -61,45 +57,40 @@ void go_to_north_west_corner()
 bool check_path() // Checks whether the path leads the way Charles is facing. If not, Charles will turn around.
 {
 	bool return_value;
-	if(!in_front_of_wall()) // The path cannot lead to a wall
+	if (!in_front_of_wall()) // The path cannot lead to a wall
 	{
 		step(); // Move one step ahead
-		if(on_ball()) // and check whether the path continues
+		if (on_ball()) // and check whether the path continues
 		{
 			return_value = true;
-		}
-		else // otherwise, turn around and return to the path corner
-		{
+		} else {
+			// otherwise, turn around and return to the path corner
 			return_value = false;
 			turn_around();
 			step();
 		}
-	}
-	else
-	{
+	} else {
 		turn_around();
 		return_value = false;
 	}
 	return return_value;
 }
 
-void follow_path()
-{
-	while(true)
-	{
-		while(on_ball() && !in_front_of_wall()) // Walk along the line of balls
+void follow_path() {
+	while (true) {
+		while (on_ball() && !in_front_of_wall()) // Walk along the line of balls
 		{
 			step();
 		}
-		if(!on_ball()) // Move one back, which places Charles on a corner, facing backwards
+		if (!on_ball()) // Move one back, which places Charles on a corner, facing backwards
 		{
 			turn_around();
 			step();
 		}
 		turn_left();
-		if(!check_path()) // Check whether the path continues to the right, if not:
+		if (!check_path()) // Check whether the path continues to the right, if not:
 		{
-			if(!check_path()) // Check whether the path continues to the left, if not:
+			if (!check_path()) // Check whether the path continues to the left, if not:
 			{
 				break;
 				// We are at the end of the line
@@ -108,20 +99,18 @@ void follow_path()
 	}
 }
 
-void hansl_and_gretl()
-{
+void hansl_and_gretl() {
 	reset();
 	make_path_with_balls();
 	follow_path();
 }
 
-void fill_cave_with_balls()
-{
+void fill_cave_with_balls() {
 	step();
-	while(!in_front_of_wall()) // While not in front of the right (or left) boundary
+	while (!in_front_of_wall()) // While not in front of the right (or left) boundary
 	{
 		turn_right();
-		while(!in_front_of_wall()) // Put balls until the cave wall is met
+		while (!in_front_of_wall()) // Put balls until the cave wall is met
 		{
 			put_ball();
 			step();
@@ -137,24 +126,21 @@ void fill_cave_with_balls()
 	turn_right();
 }
 
-void cave()
-{
+void cave() {
 	fill_cave_with_balls(); // Fill the top and move to the bottom
 	fill_cave_with_balls(); // Fill the bottom and return
 }
 
-void start_cave()
-{
+void start_cave() {
 	reset();
 	make_cave();
 	cave();
 }
 
-void rondje_om_de_kerk()
-{
+void rondje_om_de_kerk() {
 	reset();
 	make_church();
-	while(!on_ball()) // Walk to the ball marking the starting position of the church
+	while (!on_ball()) // Walk to the ball marking the starting position of the church
 	{
 		step();
 	}
@@ -167,13 +153,11 @@ void rondje_om_de_kerk()
 	step(); // Move one step ahead to avoid starting on top of the ball
 	turn_right();
 
-	while(!on_ball()) // Perimeter-walking algorithm loops until the starting ball is found
+	while (!on_ball()) // Perimeter-walking algorithm loops until the starting ball is found
 	{
-		while(in_front_of_wall())
-		{
+		while (in_front_of_wall()) {
 			turn_left();
-			if(!in_front_of_wall())
-			{
+			if (!in_front_of_wall()) {
 				step();
 				turn_right();
 			}
@@ -186,24 +170,24 @@ void rondje_om_de_kerk()
 	go_to_north_west_corner();
 }
 
-// For testing purposes, you can define your own function here:
-void test()
-{
+void go_to_middle_of_world() {
 	for (int i = 0; i <= (WereldBreedte / 2); i++)
 		step();
 	turn_right();
 	for (int i = 0; i <= (WereldHoogte / 2); i++)
 		step();
-	while(!in_front_of_wall())
-	{
+	
+}
+
+// For testing purposes, you can define your own function here:
+void test() {
+	go_to_middle_of_world();
+	while (!in_front_of_wall()) {
 		step();
-		if (on_ball())
-		{
+		if (on_ball()) {
 			get_ball();
 			turn_left();
-		}
-		else
-		{
+		} else {
 			put_ball();
 			turn_right();
 		}
@@ -215,42 +199,51 @@ void test()
 // =========================================================================
 
 
-void instant() { rest(0); };
-void quick  () { rest(    1); };
-void normal () { rest(dInit); };
-void slow   () { rest(  250); };
-void very_slow  () { rest( 1000); };
+void instant() {
+	rest(0);
+};
 
-int WINAPI WinMain (HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine, int iCmdShow)
-{
-    Menu charles_menu ((char*)"Charles") ;
-    charles_menu.add ((char*)"Clean", reset)
-                .add ((char*)"Stop",  stop) ;
+void quick() {
+	rest(1);
+};
 
-	Menu a1_menu ((char*)"Assignment 2");
-	a1_menu.add ((char*)"Hansl and Gretl", hansl_and_gretl )
-		   .add ((char*)"Cave", start_cave )
-		   .add ((char*)"Bonus: rondje om de kerk...", rondje_om_de_kerk )
-	       .add ((char*)"Test a function",test);
+void normal() {
+	rest(dInit);
+};
 
-	Menu sn_menu ((char*)"Velocity");
+void slow() {
+	rest(250);
+};
+
+void very_slow() {
+	rest(1000);
+};
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine, int iCmdShow) {
+	Menu charles_menu((char*)"Charles");
+	charles_menu.add((char*)"Clean", reset)
+	            .add((char*)"Stop", stop) ;
+
+	Menu a1_menu((char*)"Assignment 2");
+	a1_menu.add((char*)"Hansl and Gretl", hansl_and_gretl)
+	       .add((char*)"Cave", start_cave)
+	       .add((char*)"Bonus: rondje om de kerk...", rondje_om_de_kerk)
+	       .add((char*)"Test a function", test);
+
+	Menu sn_menu((char*)"Velocity");
 	sn_menu.add((char*)"Instant", instant)
-		   .add ((char*)"Quick", quick)
-		   .add ((char*)"Normal",normal)
-		   .add ((char*)"Slow",slow)
-		   .add ((char*)"Very slow",very_slow);
+	       .add((char*)"Quick", quick)
+	       .add((char*)"Normal", normal)
+	       .add((char*)"Slow", slow)
+	       .add((char*)"Very slow", very_slow);
 
-	try
-	{
-		karelsWereld().Run (charles_menu,WINARGS(hInstance, hPrevInstance, szCmdLine, iCmdShow));
+	try {
+		karelsWereld().Run(charles_menu, WINARGS(hInstance, hPrevInstance, szCmdLine, iCmdShow));
+	} catch (IllegaleActie dezeIllegaleActie) {
+		dezeIllegaleActie.report();
 	}
-	catch (IllegaleActie dezeIllegaleActie )
-	{
-		dezeIllegaleActie.report ();
-	}
-	catch (...)
-	{
-		makeAlert ("Something went terribly wrong!");
+	catch (...) {
+		makeAlert("Something went terribly wrong!");
 	}
 
 	return 0;
