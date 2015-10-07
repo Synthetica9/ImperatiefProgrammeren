0783850857e043d705127cf20a6572589a83e023
Fons van der Plas <fonsvdplas@gmail.com>
2015-10-07 17:44:55 +0200

    Added post-condition, reformatting and fixed r=0 error

--- Assignment_5/main.cpp
+++ Assignment_5/main.cpp
@@ -5,84 +5,84 @@
 
 using namespace std;
 
-enum Action {Encrypt, Decrypt};
+enum Action { Encrypt, Decrypt };
 
 int seed = 0;
 
 void initialise_pseudo_random(int r) {
 	//  pre-condition:
-	assert (r > 0 && r <= 65536) ;
+	assert(r > 0 && r <= 65536);
 	/*  post-condition:
-    seed has value r.
-*/
-	seed = r ;
+	seed has value r.*/
+	seed = r;
 }
 
 int next_pseudo_random_number() {
 	//  pre-condition:
-	assert (seed > 0 && seed <= 65536) ;
+	assert(seed > 0 && seed <= 65536);
 	/*  post-condition:
-    result value > 0 and result value <= 65536 and result value != seed at entry of function
-*/
+	result value > 0 and result value <= 65536 and result value != seed at entry of function*/
 	const int seed75 = seed * 75;
 	int next = (seed75 & 65535) - (seed75 >> 16);
-	if (next < 0)
-		next += 65537 ;
-	seed = next ;
-	return next ;
+	if(next < 0)
+		next += 65537;
+	seed = next;
+	return next;
 }
 
 template <typename T>
 bool get_input(T &out_var) {
 	cin >> out_var;
-	if (!cin) {
+	if(!cin) {
 		cin.clear();
 		cin.ignore(1000, '\n');
 		return false;
-	} else {
-		return true;
 	}
+	return true;
 }
 
 char rotate(char a, int r, Action e) {
 	//  Pre-condition:
-//	assert(r > 0 && r <= 65536 && a >= 0);
-	assert(r > 0);
-	assert(r <= 65536);
-//	assert(a > 0);
-	//  Post-condition:
-	// char b is char a rotated by r mod 128-32 places iff it is not a control character,
-	// char b is char a iff it were a control character
+	assert(r >= 0 && r <= 65536);
+	/*  Post-condition:
+	Result is char a rotated by r mod 128-32 places iff it is not a control character,
+	Result is char a iff it is a control character. */
 	char b;
-	if (a < 32) {
+	if(a < 32) {
 		b = a;
 	} else {
-		if (e == Encrypt) {
+		if(e == Encrypt) {
 			b = (a - 32 + (r % (128 - 32)) + (128 - 32)) % (128 - 32) + 32;
-		} else if (e == Decrypt) {
+		} else if(e == Decrypt) {
 			b = (a - 32 - (r % (128 - 32)) + (128 - 32)) % (128 - 32) + 32;
 		} else {
-			assert(false); // This shouldn't ever happen
+			assert(false); // This should never happen
 			b = -1;
 		}
 	}
 	return b;
 }
 
-const int test_r[] = { 1, 42, 96, 65535 };
+const int test_r[] = {1, 42, 96, 65535};
 void test_rotate() {
-	//  Pre-conditie:
+	//  Pre-condition:
 	assert(true);
-	//  Post-conditie:
-	// vul hier je post-conditie in
+	/*  Post-condition:
+	For non-negative values the function emits for each ASCII display character a, a line of output containing: 
+	the character a, 
+	the value of rotate(a, r, Encrypt), and 
+	the value of rotate(rotate(a, r, Encrypt), r, Decrypt). 
+	
+	The last value should always be equal to the first value, and the second value is always different 
+	except when r is a multiple of (128-32). */
 	int r;
-	while (true) {
+	while(true) {
 		cout << "Enter a value for r: ";
-		if (get_input(r)) {
-			if (r < 0) {
+		if(get_input(r)) {
+			if(r < 0) {
 				break;
 			}
-			for (char a = 32; a < 127; a++) {
+			for(char a = 32; a < 127; a++) {
 				cout << a << ' ' << rotate(a, r, Encrypt) << ' ' << rotate(rotate(a, r, Encrypt), r, Decrypt) << endl;
 			}
 		} else {
@@ -93,61 +93,58 @@ void test_rotate() {
 }
 
 bool open_input_and_output_file(ifstream &infile, ofstream &outfile) {
-	//  Pre-conditie:
+	//  Pre-condition:
 	assert(!infile.is_open() && !outfile.is_open());
-	//  Post-conditie:
-	// The two files will be open if the output is True.
-	// if the output is False, something went wrong and the output files aren't open
+	/*  Post-condition:
+	Result is true if both files opened succesfully,
+	result is false if opening both files failed. */
 	string infile_name, outfile_name;
-	do 
+	do
 		cout << "Enter infile name: ";
-	while (!get_input(infile_name));
+	while(!get_input(infile_name));
 
-	do 
+	do
 		cout << "Enter outfile name: ";
 	while(!get_input(outfile_name));
 
 	infile.open(infile_name);
 	outfile.open(outfile_name);
 
-	bool status = infile && outfile;
-
-
-	if (status) {
+	if(infile && outfile) {
 		cout << "Files opened succesfully" << endl;
-	} else {
-		cout << "Error while opening files" << endl;
-		infile.close();
-		outfile.close();
-		infile.clear();
-		outfile.clear();
+		return true;
 	}
-	return status;
+	cout << "Error while opening files" << endl;
+	infile.close();
+	outfile.close();
+	infile.clear();
+	outfile.clear();
+	return false;
 }
 
 void do_one_time_pad(Action mode, ifstream& infile, ofstream& outfile, int r) {
 	initialise_pseudo_random(r);
 	char current_char;
 	char plain;
-	while (infile) {
+	while(infile) {
 		infile.get(current_char);
 		plain = rotate(current_char, next_pseudo_random_number(), mode);
 		outfile.put(plain);
-//		cout << plain;
+		//cout << plain;
 	}
 }
 
 void one_time_pad() {
 	Action mode;
 	int mode_select = -1;
-	
+
 	do {
 		cout << "Select a mode; 0 = Encrypt, 1 = Decrypt: ";
-	} while (!get_input(mode_select) || mode_select < 0 || mode_select > 1);
+	} while(!get_input(mode_select) || mode_select < 0 || mode_select > 1);
 
-	if (mode_select == 0)
+	if(mode_select == 0)
 		mode = Encrypt;
-	else if (mode_select == 1)
+	else if(mode_select == 1)
 		mode = Decrypt;
 	else
 		//fuck 
@@ -156,11 +153,11 @@ void one_time_pad() {
 
 	ifstream infile;
 	ofstream outfile;
-	if (open_input_and_output_file(infile, outfile)) {
+	if(open_input_and_output_file(infile, outfile)) {
 		int r;
 		do
 			cout << "Enter a value for r: ";
-		while (!get_input(r));
+		while(!get_input(r));
 		do_one_time_pad(mode, infile, outfile, r);
 		cout << "Done!" << endl;
 		infile.close();
@@ -174,9 +171,9 @@ string read_file(string filename) {
 	infile.open(filename);
 	string return_string = "";
 	char current_char;
-	while (infile) {
+	while(infile) {
 		infile.get(current_char);
-//		cout << current_char;
+		//cout << current_char;
 		return_string += current_char;
 	}
 	infile.close();
@@ -186,25 +183,21 @@ string read_file(string filename) {
 
 
 void secret() {
-
 	// We'll hunt for spaces, because they are the most common characters in most texts
 	int best_key = 0;
 	int best_spaces = 0;
 	string input_string = read_file("secret.txt");
-	for (int key = 1; key < 65535; key++) {
+	for(int key = 1; key < 65535; key++) {
 		initialise_pseudo_random(key);
-//		cout << key << endl;
 		int num_spaces = 0;
-		char plain;
-		for (char& a : input_string) {
-			plain = rotate(a, next_pseudo_random_number(), Decrypt);
-//			cout << plain;
-			if (plain == ' ') {
+		for(char& a : input_string) {
+			char plain = rotate(a, next_pseudo_random_number(), Decrypt);
+			if(plain == ' ') {
 				num_spaces++;
 			}
 		}
-		if (num_spaces > best_spaces){
-			cout << "Found new best key: " << key  << ", with " << num_spaces << " spaces." << endl;
+		if(num_spaces > best_spaces) {
+			cout << "Found new best key: " << key << ", with " << num_spaces << " spaces." << endl;
 			best_spaces = num_spaces;
 			best_key = key;
 		}
@@ -221,21 +214,21 @@ void secret() {
 
 int main() {
 	int choice = -1;
-	while (choice != 4) {
+	while(choice != 4) {
 		cout
 			<< "1: Test the rotation function" << endl
-			<< "2: En- or decrypt a file." << endl
+			<< "2: Encrypt or decrypt a file." << endl
 			<< "3: Crack the secret code!" << endl
 			<< "4: Quit." << endl
 			<< "Your choice? ";
-		if (get_input(choice)) {
-			if (choice == 1)
+		if(get_input(choice)) {
+			if(choice == 1)
 				test_rotate();
-			else if (choice == 2)
+			else if(choice == 2)
 				one_time_pad();
-			else if (choice == 3)
+			else if(choice == 3)
 				secret();
-			else if (choice == 4)
+			else if(choice == 4)
 				// We don't need to break, because of the condition of the loop
 				cout << "Bye!" << endl;
 			else
